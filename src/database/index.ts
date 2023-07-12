c838c7598510b1b0f74f50279ca57a420a2bdcc4
LeeSangMin1029 <sangmins39@gmail.com>
2023-07-12 17:58:33 +0900

    indexed 적용

---
+++ src/database/index.ts
@@ -0,0 +1,27 @@
+import { UserData, UserSchema, IDBUserSchema } from "@/@types/indexed-db";
+import { openDB } from "idb";
+
+export const getDB = async (): Promise<IDBUserSchema> =>
+  await openDB<UserSchema>("youtube", 1, {
+    upgrade: (db: IDBUserSchema) =>
+      db.createObjectStore("user", {
+        keyPath: "googleID",
+      }),
+  });
+
+export const getUser = async () => {
+  const db = await getDB();
+  const tx = db.transaction("user", "readonly");
+  const user = tx.objectStore("user");
+  const req = await user.openCursor();
+  return req?.value;
+};
+
+export const addUser = async (key: string, data: UserData) => {
+  const db = await getDB();
+  const tx = db.transaction("user", "readwrite");
+  const store = tx.objectStore("user");
+  const req = await store.openCursor(key);
+  if (req?.key !== key) return await store.add(data);
+  db.close();
+};
